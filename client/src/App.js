04f85d126807232e0b7ce6f7a11983e7e429ce8b
minfromdavis <minchin903@gmail.com>
2024-03-07 10:10:13 -0800

    added contact link

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import './App.css';
 import Home from './components/home'
-
+import Contact from './components/contact'
 
 
 function App() {
@@ -11,11 +11,13 @@ function App() {
       <nav>
         <h1>Washingtown</h1>
         <NavLink to="/">home</NavLink>
+        <NavLink to="contact">contact</NavLink>
       </nav>
       </header>
       <main>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
       </main>
    </BrowserRouter>
