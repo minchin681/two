aa97c0ac2b838290e7484b9393bac29382c25137
minfromdavis <minchin903@gmail.com>
2024-03-07 09:14:43 -0800

    testing 1234

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import './App.css';
-import Home from './components/home'
 
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-     <header>
-      <nav>
-        <h1>Washingtown</h1>
-        <NavLink to="/">home</NavLink>
-        <NavLink to="contact">contact</NavLink>
-      </nav>
-     </header>
-     <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-     </main>
-    </BrowserRouter>
+    <div>
+      <h2>testing 1234</h2>
+    </div>
+    
   );
 }
 
